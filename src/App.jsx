784633a24c92309059f8d73a8e0957ff9980658e
utkarsh-radiant <utkarsh@radiantinfonet.com>
2024-04-17 10:06:37 +0530

    done 2 change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const userName = import.meta.env.VITE_NAME;
   return (
     <>
-      My name is Utkarsh kushwaha
+      My name is Utkarsh kushwaha changes done.
       <h1>VITE_USER : {userEmail || "Not found"}</h1>
       <h1>VITE_NAME : {userName || "Not found"}</h1>
     </>
